393344a0524639a7fcb086ca492378050802a61f
SamuelNunesDev <sammoc11@hotmail.com>
2021-06-22 11:18:42 -0300

    commit inicial

---
+++ Exercícios/Ex08/Ex08.js
@@ -0,0 +1,11 @@
+console.log('Testando o console...')
+
+var vel = 100.5
+console.log(`A velocidade do seu carro atingiu ${vel}km/h`)
+if(vel >= 100)
+{
+    console.log('Você ultrapassou a velocidade máxima permitida!')
+}else 
+{
+    console.log('Dirija sempre com segurança, boa viagem!')
+}
