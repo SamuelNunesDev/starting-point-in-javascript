10058576f261fea0035d7fc16d81c10a2bed84da
SamuelNunesDev <sammoc11@hotmail.com>
2021-06-23 10:45:47 -0300

    commit inicial

--- Exercícios/Ex15/script.js
+++ Exercícios/Ex15/script.js
@@ -4,21 +4,29 @@ function contar()
     var fim = document.querySelector('#fim')
     var passo = document.querySelector('#passo')
     var res = document.querySelector('#res')
-    res.innerText = ''
+    res.innerText = 'Contando: '
     
     if (String(inicio.value).length == 0 || String(fim.value).length == 0 || String(passo.value).length == 0)
     {
         res.innerText = 'Impossível contar! Preencha todos os campos e tente novamente!'
-    } else
+    } 
+    else
     {
-        if (Number(passo.value) == 0)
+        if (Number(passo.value) <= 0)
         {
-            alert('Não é possível contar de zero em zero! Considerando PASSO 1!')
+            alert('Passo inválido! Considerando PASSO 1!')
             passo.value = 1
+        } 
+        else if (Number(inicio.value) > Number(fim.value))
+        {
+            for (var c = Number(inicio.value); c >= Number(fim.value); c -= Number(passo.value))
+            {
+                res.innerText += ` ${c} 👉 `
+            }
         }
         for (var c = Number(inicio.value); c <= Number(fim.value); c += Number(passo.value))
         {
-            res.innerText += `${c} 👉 `
+            res.innerText += ` ${c} 👉 `
         }
         res.innerText += '🏁'
     }
